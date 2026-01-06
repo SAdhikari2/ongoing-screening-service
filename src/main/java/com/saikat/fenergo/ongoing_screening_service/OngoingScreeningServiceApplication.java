16d68344e2d797443175222763dba0c59557a431
Saikat Adhikari <saikat.adhikari1994@gmail.com>
2026-01-06 09:14:35 +0530

    Initial commit

---
+++ src/main/java/com/saikat/fenergo/ongoing_screening_service/OngoingScreeningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saikat.fenergo.ongoing_screening_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngoingScreeningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngoingScreeningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saikat/fenergo/ongoing_screening_service/OngoingScreeningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saikat.fenergo.ongoing_screening_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngoingScreeningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
